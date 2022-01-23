28fae157acdc2dba7aef188bd7341ca00d9522a3
a.artikov <a.artikov@gmail.com>
2022-01-23 17:47:01 +0300

    Add devtools client module

---
+++ replica-devtools-client/src/main/kotlin/me/aartikov/replica/devtools/client/main.kt
@@ -0,0 +1,30 @@
+package me.aartikov.replica.devtools.client
+
+import androidx.compose.runtime.*
+import org.jetbrains.compose.web.dom.Button
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.Text
+import org.jetbrains.compose.web.renderComposable
+
+fun main() {
+    renderComposable(rootElementId = "root") {
+        Body()
+    }
+}
+
+@Composable
+fun Body() {
+    var counter by remember { mutableStateOf(0) }
+    Div {
+        Text("Clicked: $counter")
+    }
+    Button(
+        attrs = {
+            onClick {
+                counter++
+            }
+        }
+    ) {
+        Text("Click")
+    }
+}
