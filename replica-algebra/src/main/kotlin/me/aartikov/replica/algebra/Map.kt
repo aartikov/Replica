e102e5dd36bf32ea9b12add07aa2ceed7ef3919f
Artur Artikov <artur.artikov@mobileup.ru>
2022-01-24 18:16:20 +0300

    Add result caching for combine and map

--- replica-algebra/src/main/kotlin/me/aartikov/replica/algebra/Map.kt
+++ replica-algebra/src/main/kotlin/me/aartikov/replica/algebra/Map.kt
@@ -48,6 +48,12 @@ private class MappedReplica<T : Any, R : Any>(
     }
 }
 
+private class MappingResult<T : Any, R : Any>(
+    val originalData: T?,
+    val data: R?,
+    val error: Exception?
+)
+
 private class MappedReplicaObserver<T : Any, R : Any>(
     private val coroutineScope: CoroutineScope,
     activeFlow: StateFlow<Boolean>,
@@ -74,32 +80,44 @@ private class MappedReplicaObserver<T : Any, R : Any>(
     }
 
     private fun launchStateObserving() {
+        var cachedMappingResult: MappingResult<T, R>? = null
         stateObservingJob = originalObserver.stateFlow
             .onEach { state ->
-                var mappedData: R? = null
-                var mappingError: Exception? = null
-
-                try {
-                    mappedData = state.data?.let(transform)
-                } catch (e: Exception) {
-                    mappingError = e
-                }
+                val mappingResult = mapState(state, cachedMappingResult)
+                cachedMappingResult = mappingResult
 
                 _stateFlow.value = Loadable(
                     loading = state.loading,
-                    data = mappedData,
-                    error = if (mappingError != null) {
-                        CombinedLoadingError(mappingError)
+                    data = mappingResult.data,
+                    error = if (mappingResult.error != null) {
+                        CombinedLoadingError(mappingResult.error)
                     } else {
                         state.error
                     }
                 )
 
-                if (mappingError != null && !state.loading) {
-                    _loadingErrorFlow.emit(LoadingError(mappingError))
+                if (mappingResult.error != null && !state.loading) {
+                    _loadingErrorFlow.emit(LoadingError(mappingResult.error))
                 }
             }
             .launchIn(coroutineScope)
+    }
+
+    private fun mapState(
+        state: Loadable<T>,
+        cachedResult: MappingResult<T, R>?
+    ): MappingResult<T, R> {
+        val originalData = state.data
+        if (cachedResult != null && originalData == cachedResult.originalData) {
+            return cachedResult
+        }
+
+        return try {
+            val mappedData = originalData?.let(transform)
+            MappingResult(originalData, mappedData, null)
+        } catch (e: Exception) {
+            MappingResult(originalData, null, e)
+        }
     }
 
     private fun launchLoadingErrorsObserving() {
