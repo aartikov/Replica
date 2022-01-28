c1017e8a0edb247a736331fbec1516cbe6f8b90b
Yegor Belov <egor.belov@mobileup.ru>
2022-01-28 15:25:20 +0300

    New icons for ObserverIcon.kt

--- replica-devtools-client/src/main/kotlin/me/aartikov/replica/devtools/client/ObserverIcon.kt
+++ replica-devtools-client/src/main/kotlin/me/aartikov/replica/devtools/client/ObserverIcon.kt
@@ -2,21 +2,34 @@ package me.aartikov.replica.devtools.client
 
 import androidx.compose.runtime.Composable
 import me.aartikov.replica.devtools.client.view_data.ObserverType
+import org.jetbrains.compose.web.css.height
 import org.jetbrains.compose.web.css.marginLeft
 import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.css.width
+import org.jetbrains.compose.web.dom.Img
 
 @Composable
 fun ObserverIcon(type: ObserverType) {
-    MaterialIcon(
-        name = when (type) {
-            ObserverType.Active -> "visibility"
-            ObserverType.Inactive -> "visibility_off"
-            ObserverType.None -> ""
-        },
+    Container(
         attrs = {
             style {
                 marginLeft(16.px)
+                height(24.px)
+                width(24.px)
             }
         }
-    )
+    ) {
+        val isDarkTheme = LocalTheme.current.isDark
+        when (type) {
+            ObserverType.Active -> Img(
+                src = if (isDarkTheme) "ic_eye_24_white.png" else "ic_eye_24_black.png"
+            )
+            ObserverType.Inactive -> Img(
+                src = if (isDarkTheme) "ic_closed_eye_24_white.png" else "ic_closed_eye_24_black.png"
+            )
+            ObserverType.None -> {
+                // Nothing
+            }
+        }
+    }
 }
