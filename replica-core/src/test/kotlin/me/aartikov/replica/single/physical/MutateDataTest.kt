929d7685464fb9769a112b5d42ba79bd7730c742
bobrusik <egorius.belov@yandex.com>
2022-02-09 10:58:17 +0300

    Tests for PhycicalReplica methods

---
+++ replica-core/src/test/kotlin/me/aartikov/replica/single/physical/MutateDataTest.kt
@@ -0,0 +1,86 @@
+package me.aartikov.replica.single.physical
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
+import me.aartikov.replica.single.currentState
+import me.aartikov.replica.single.utils.LoadingFailedException
+import me.aartikov.replica.single.utils.MainCoroutineRule
+import me.aartikov.replica.single.utils.ReplicaProvider
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
+import org.junit.Rule
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class MutateDataTest {
+
+    private val replicaProvider = ReplicaProvider()
+
+    companion object {
+        private const val DEFAULT_DELAY = 100L
+    }
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @Test
+    fun `doesn't mutate empty data`() = runTest {
+        val newData = "new data"
+        val replica = replicaProvider.replica()
+
+        replica.mutateData { newData }
+
+        assertNull(replica.currentState.data?.value)
+    }
+
+    @Test
+    fun `doesn't mutate empty data with error state`() = runTest {
+        val newData = "new data"
+        val replica = replicaProvider.replica(
+            fetcher = { throw LoadingFailedException()}
+        )
+
+        replica.refresh()
+        runCurrent()
+        replica.mutateData { newData }
+
+        assertNull(replica.currentState.data?.value)
+    }
+
+    @Test
+    fun `mutates existing data`() = runTest {
+        val newData = "new data"
+        val replica = replicaProvider.replica()
+
+        replica.refresh()
+        runCurrent()
+        replica.mutateData { it + newData }
+
+        assertEquals(ReplicaProvider.TEST_DATA + newData, replica.currentState.data?.value)
+    }
+
+    @Test
+    fun `mutates data call doesn't cancel loading`() = runTest {
+        val newData = "new data"
+        val replica = replicaProvider.replica(
+            fetcher = {
+                delay(DEFAULT_DELAY)
+                "test"
+            }
+        )
+
+        replica.setData("initial data")
+        replica.refresh()
+        delay(DEFAULT_DELAY / 2)
+        replica.mutateData { newData }
+        val intermediateState = replica.currentState
+        delay((DEFAULT_DELAY / 2) + 1) // loading is completed
+
+        assertEquals(newData, intermediateState.data?.value)
+        assertTrue(newData, intermediateState.loading)
+        assertEquals("test", replica.currentState.data?.value)
+    }
+}
