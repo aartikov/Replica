a70ed7afb37ad3eb0c5ac661f179f474fd19cb77
bobrusik <egorius.belov@yandex.com>
2022-02-09 11:08:20 +0300

    Clean up tests

--- replica-core/src/test/kotlin/me/aartikov/replica/single/RevalidateTest.kt
+++ replica-core/src/test/kotlin/me/aartikov/replica/single/RevalidateTest.kt
@@ -47,8 +47,9 @@ class RevalidateTest {
         )
 
         replica.revalidate()
-        replica.cancel()
         runCurrent()
+        replica.cancel()
+        delay(DEFAULT_DELAY * 2) // waiting until loading time is complete
 
         assertNull(replica.currentState.data)
     }
@@ -70,12 +71,12 @@ class RevalidateTest {
 
         replica.revalidate()
         runCurrent()
-        val previousData = replica.currentState
+        val previousState = replica.currentState
         replica.revalidate()
         runCurrent()
 
-        assertEquals(ReplicaProvider.TEST_DATA, previousData.data?.value)
-        assertTrue(previousData.hasFreshData)
+        assertEquals(ReplicaProvider.TEST_DATA, previousState.data?.value)
+        assertTrue(previousState.hasFreshData)
         val currentState = replica.currentState
         assertEquals(ReplicaProvider.TEST_DATA, currentState.data?.value)
     }
