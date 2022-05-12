f3c41b19e1896c44f6058980d6b0ed9118ed67ad
Artur Artikov <artur.artikov@mobileup.ru>
2022-05-12 19:02:50 +0300

    Fix keepPreviousData

--- replica-core/src/main/kotlin/me/aartikov/replica/keyed/internal/KeepPreviousData.kt
+++ replica-core/src/main/kotlin/me/aartikov/replica/keyed/internal/KeepPreviousData.kt
@@ -1,30 +1,47 @@
 package me.aartikov.replica.keyed.internal
 
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import me.aartikov.replica.single.Loadable
 
-@OptIn(FlowPreview::class)
 internal fun <T : Any> StateFlow<Loadable<T>>.keepPreviousData(
     coroutineScope: CoroutineScope
 ): StateFlow<Loadable<T>> {
-    val result = MutableStateFlow(this.value)
+    val result = MutableStateFlow(value)
+    var previousData: T? = value.data
+    var previousDataCleanupJob: Job? = null
+
     this
-        .debounce { newValue ->
-            if (newValue.data == null && !newValue.loading) {
-                100 // wait until loading will started for a just created replica
-            } else {
-                0
-            }
-        }
         .onEach { newValue ->
+            if (newValue.data != null) {
+                previousData = newValue.data
+            }
+
+            if (newValue.data == null && !newValue.loading) {
+                previousDataCleanupJob?.cancel()
+                previousDataCleanupJob = coroutineScope.launch {
+                    delay(100)
+                    previousData = null
+                    previousDataCleanupJob = null
+                }
+            } else if (previousDataCleanupJob != null) {
+                previousDataCleanupJob?.cancel()
+                previousDataCleanupJob = null
+            }
+
             result.value = if (newValue.data == null && newValue.loading) {
-                newValue.copy(data = result.value.data)
+                newValue.copy(data = previousData)
             } else {
                 newValue
             }
-        }.launchIn(coroutineScope)
+        }
+        .launchIn(coroutineScope)
 
     return result
 }
