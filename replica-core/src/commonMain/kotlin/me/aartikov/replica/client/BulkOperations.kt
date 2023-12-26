3aca94fa2b1e4ad4b777ba959bbbfa9165844b52
Artur Artikov <artur.artikov@mobileup.ru>
2023-12-26 13:01:35 +0300

    Add bulk operations: cancelByTag, clearByTag, invalidateByTag

--- replica-core/src/commonMain/kotlin/me/aartikov/replica/client/BulkOperations.kt
+++ replica-core/src/commonMain/kotlin/me/aartikov/replica/client/BulkOperations.kt
@@ -2,7 +2,11 @@ package me.aartikov.replica.client
 
 import me.aartikov.replica.common.InvalidationMode
 import me.aartikov.replica.common.ReplicaTag
-import me.aartikov.replica.keyed.*
+import me.aartikov.replica.keyed.cancelAll
+import me.aartikov.replica.keyed.cancelByTags
+import me.aartikov.replica.keyed.clearByTags
+import me.aartikov.replica.keyed.invalidateAll
+import me.aartikov.replica.keyed.invalidateByTags
 
 /**
  * Cancels network requests in all replicas.
@@ -95,4 +99,30 @@ suspend fun ReplicaClient.invalidateByTags(
     onEachKeyedReplica {
         this.invalidateByTags(mode, predicate)
     }
+}
+
+/**
+ * Cancels network requests in replicas with a given [tag].
+ */
+suspend fun ReplicaClient.cancelByTag(tag: ReplicaTag) {
+    cancelByTags { it.contains(tag) }
+}
+
+/**
+ * Cancels network requests and clears data in replicas with a given [tag].
+ */
+suspend fun ReplicaClient.clearByTag(tag: ReplicaTag) {
+    clearByTags { it.contains(tag) }
+}
+
+/**
+ * Makes replicas with a given [tag] stale.
+ *
+ * @param mode specifies how replicas behave after invalidation. See: [InvalidationMode].
+ */
+suspend fun ReplicaClient.invalidateByTag(
+    mode: InvalidationMode = InvalidationMode.RefreshIfHasObservers,
+    tag: ReplicaTag
+) {
+    invalidateByTags(mode) { it.contains(tag) }
 }
